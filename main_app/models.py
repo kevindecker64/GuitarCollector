47260e97e9afa50b1456a5c626647d34efb7a0f1
Kevin Decker <KevinDecker64@gmail.com>
2021-05-17 14:09:05 -0500

    creates Guitar model and fills Database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Guitar(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.model
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Guitar:
-    def __init__(self, make, model, year, description):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.description = description
-
-guitars = [
-    Guitar('Gibson', 'Ripper', 1981, 'Smooth as butter'),
-    Guitar('Fender', 'P-Bass', 2013, 'Phat AF'),
-    Guitar('Warwick', 'Corvette', 2006, 'Fretless'),
-]
+from .models import Guitar
 
 def home(request):
-    return HttpResponse('<h1>Hello, get some guitboxes</h1>')
+    return render(request, 'home.html')
     
 def about(request):
     return render(request, 'about.html')
 
 def guitars_index(request):
+    guitars = Guitar.objects.all()
     return render(request, 'guitars/index.html', {'guitars': guitars})
